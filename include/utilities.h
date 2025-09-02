a8c2f9fe7bda883410d493a0a55128339f2327e7
jojohuerta <johuerta@itba.edu.ar>
2025-09-02 15:29:45 -0300

    Code Cleanup #2

--- include/utilities.h
+++ include/utilities.h
@@ -4,5 +4,8 @@
 #include "shmConstants.h"
 
 void interpretMovement(unsigned char mov, boardGameState *shm_bgs, int player);
+void initRandom();
+int getSquareValue();
+unsigned char movAnalysis();
 
 #endif // UTILITIES_H
