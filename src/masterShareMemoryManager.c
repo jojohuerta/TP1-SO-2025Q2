c535ba074f816554d941ba1f0600d274d134f368
ifrund <ifrund@itba.edu.ar>
2025-09-12 16:50:43 -0300

    shm_open permissions

--- src/masterShareMemoryManager.c
+++ src/masterShareMemoryManager.c
@@ -1,10 +1,3 @@
-//#include <ctype.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <stddef.h> 
-//#include <string.h>
-//#include <time.h>
-
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -16,14 +9,15 @@
 #include "../include/errorHandling.h"
 
 // --- Game state shm --- //
-boardGameState * createShmBoardGameState(int boardWidth, int boardHeight, int playerAmount, unsigned int seed){
+boardGameState *createShmBoardGameState(int boardWidth, int boardHeight, int playerAmount, unsigned int seed)
+{
     int shm_fd;
-    char * shm_path = GAME_STATE_PATH;
-    boardGameState * shm_addr;
-    srand(seed); 
+    char *shm_path = GAME_STATE_PATH;
+    boardGameState *shm_addr;
+    srand(seed);
 
     // - Create game state shm - //
-    shm_fd = shm_open(shm_path, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
+    shm_fd = shm_open(shm_path, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR + S_IWUSR);
     if (shm_fd == -1)
         errExit("Uncaught error: failed to create game state shared memory");
 
@@ -49,33 +43,38 @@ boardGameState * createShmBoardGameState(int boardWidth, int boardHeight, int pl
     memset(shm_addr->players, 0, sizeof(shm_addr->players));
 
     // - Set rewards - //
-    for (int i = 0; i < boardHeight * boardWidth; i++) {
+    for (int i = 0; i < boardHeight * boardWidth; i++)
+    {
         shm_addr->boardStart[i] = (rand() % 8) + 1;
     }
 
     return shm_addr;
 }
 
-void closeShmBoardGameState(boardGameState * shm_addr){
+void closeShmBoardGameState(boardGameState *shm_addr)
+{
 
     int boardGameStateSize = sizeof(boardGameState) + sizeof(int) * (shm_addr->boardWidth * shm_addr->boardHeight);
 
-    if (munmap(shm_addr, boardGameStateSize) == -1) {
+    if (munmap(shm_addr, boardGameStateSize) == -1)
+    {
         errExit("Uncaught error: failed to unmap game state shared memory");
     }
 
-    if (shm_unlink(GAME_STATE_PATH) == -1) {
+    if (shm_unlink(GAME_STATE_PATH) == -1)
+    {
         errExit("Uncaught error: failed to unlink game state shared memory");
     }
 }
 
-syncState* createShmSyncState(){
+syncState *createShmSyncState()
+{
     int shm_fd;
-    char * shm_path = SYNC_STATE_PATH;
-    syncState * shm_addr;
+    char *shm_path = SYNC_STATE_PATH;
+    syncState *shm_addr;
 
     // - Create sync state shm - //
-    shm_fd = shm_open(shm_path, O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
+    shm_fd = shm_open(shm_path, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR + S_IWUSR);
     if (shm_fd == -1)
         errExit("Uncaught error: failed to create sync state shared memory");
 
@@ -90,16 +89,22 @@ syncState* createShmSyncState(){
 
     // - Close shm's fd after mapping - //
     close(shm_fd);
-    
+
     // - Initialize shm - //
-    if (sem_init(&shm_addr->view_print_pending_sem, 1, 0) == -1) errExit("Uncaught error: failed to initialize print pending semaphore");
-    if (sem_init(&shm_addr->view_print_done_sem, 1, 0) == -1) errExit("Uncaught error: failed to initialize print done semaphore");
-    if (sem_init(&shm_addr->game_state_starvation_mutex, 1, 1) == -1) errExit("Uncaught error: failed to initialize game state starvation semaphore");
-    if (sem_init(&shm_addr->game_state_mutex, 1, 1) == -1) errExit("Uncaught error: failed to initialize game state semaphore");
-    if (sem_init(&shm_addr->reader_count_mutex, 1, 1) == -1) errExit("Uncaught error: failed to initialize readers count semaphore");
+    if (sem_init(&shm_addr->view_print_pending_sem, 1, 0) == -1)
+        errExit("Uncaught error: failed to initialize print pending semaphore");
+    if (sem_init(&shm_addr->view_print_done_sem, 1, 0) == -1)
+        errExit("Uncaught error: failed to initialize print done semaphore");
+    if (sem_init(&shm_addr->game_state_starvation_mutex, 1, 1) == -1)
+        errExit("Uncaught error: failed to initialize game state starvation semaphore");
+    if (sem_init(&shm_addr->game_state_mutex, 1, 1) == -1)
+        errExit("Uncaught error: failed to initialize game state semaphore");
+    if (sem_init(&shm_addr->reader_count_mutex, 1, 1) == -1)
+        errExit("Uncaught error: failed to initialize readers count semaphore");
     shm_addr->reader_count = 0;
-    
-    for (int i = 0; i < 9; i++) {
+
+    for (int i = 0; i < 9; i++)
+    {
         if (sem_init(&shm_addr->player_can_move_sem[i], 1, 0) == -1)
             errExit("Uncaught error: failed to initialize player can move semaphores");
     }
@@ -107,13 +112,16 @@ syncState* createShmSyncState(){
     return shm_addr;
 }
 
-void closeShmSyncState(syncState * shm_addr){
-    
-    if (munmap(shm_addr, SYNC_STATE_SIZE) == -1) {
+void closeShmSyncState(syncState *shm_addr)
+{
+
+    if (munmap(shm_addr, SYNC_STATE_SIZE) == -1)
+    {
         errExit("Uncaught error: failed to unmap sync state shared memory");
     }
 
-    if (shm_unlink(SYNC_STATE_PATH) == -1) {
+    if (shm_unlink(SYNC_STATE_PATH) == -1)
+    {
         errExit("Uncaught error: failed to unlink sync state shared memory");
     }
 }
